f3cd9ef10d7de39bf5a809601ef2738a26ce7b09
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-02-19 07:10:36 -0400

    Faça como eu fiz: Novo cadastro de fornecedor

--- api-petshop/api/rotas/fornecedores/Rotas.v2.js
+++ api-petshop/api/rotas/fornecedores/Rotas.v2.js
@@ -1,9 +1,10 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')  
 const SerializadorFornecedor = require('../../Serializador').SerializadorFornecedor
+const Fornecedor = require('./Fornecedor')
 
 roteador.options('/', (requisicao, resposta) => {
-    resposta.set('Access-Control-Allow-Methods', 'GET')
+    resposta.set('Access-Control-Allow-Methods', 'GET, POST')
     resposta.set('Access-Control-Allow-Headers', 'Content-Type')
     resposta.status(204)
     resposta.end()
@@ -18,6 +19,23 @@ roteador.get('/', async (requisicao, resposta) => {
     resposta.send(
         serializador.serializar(resultados)
     )
+})
+
+roteador.post('/', async (requisicao, resposta, proximo) => {
+    try {
+        const dadosRecebidos = requisicao.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        resposta.status(201)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type')
+        )
+        resposta.send(
+            serializador.serializar(fornecedor)
+        )
+    } catch (erro) {
+        proximo(erro)
+    }
 })
 
 module.exports = roteador
